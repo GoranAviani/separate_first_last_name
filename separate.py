af676f855532315f3031c71a01bc0e170d1c8a31
goranaviani <goranaviani@gmail.com>
2019-02-23 17:35:59 +0100

    t

--- separate.py
+++ separate.py
@@ -24,13 +24,30 @@
 
 def separate_names(column1, column2, result_column1, result_column2):
 
-    for index, x in enumerate(column1):
-       if len(x) > 1: ## fist and last name are in column2  # greather than 1 character
-           result_column1.append(x)
-       else: #if there is no first name
-           if len(column2[index]) > 1:
-               namesList = split_string(column2[index]) # list of all words in column 2
-            # TODO finish
+    for index, x in enumerate(column2):
+        if len(column2[index]) > 1 and len(column2[index]) > 1:  #if all is ok # greather than 1 character
+            result_column2.append(column2[index])
+            result_column1.append(column1[index])
+
+        elif len(column2[index]) == 0: #if there is no last name
+            if len(column1[index]) > 1: # check if there if there is anything in first name
+
+               namesList = split_string(column1[index]) # list of all words in column 1
+               lastName = namesList.pop()
+               result_column2.append(lastName)
+               result_column1.append(' '.join(namesList))
+
+            else: #there is nothing in both names
+                print("Both first name and last name are empty for row {}" . format(index+1)) # +1 because first row is column name
+        else: #that means column1 is empty
+            if len(column2[index]) > 1:
+                namesList = split_string(column2[index])  # list of all words
+                firstName = namesList.pop(0)
+                result_column1.append(firstName)
+                result_column2.append(' '.join(namesList))
+
+
+    return result_column1, result_column2
 
 def split_string(string):
     result = string.split(" ")
@@ -40,14 +57,17 @@ def split_string(string):
 
 def main():
 
-    column1 = ['Felix','Luc Picard', '','Seven OF eight','']
-    column2 = ['Constansa', '', 'Alan Ford', '', '']
+    #these columns were provided from excel
+    column1 = ['first_name','Felix','Luc Picard', '','Seven OF eight','']
+    column2 = ['last_name','Constansa', '', 'Alan Ford', '', '']
 
     result_column1 = []
     result_column2 = []
 
-    separate_names(column1, column2, result_column1, result_column2)
+    result_column1, result_column2 = separate_names(column1, column2, result_column1, result_column2)
 
+    print(result_column1)
+    print(result_column2)
 
 if __name__ == '__main__':
     main()
